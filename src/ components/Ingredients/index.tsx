a93b47223475169f9dd42f6605e37a01ebebad83
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-04-22 21:06:18 +0200

    Update components

--- src/ components/Ingredients/index.tsx
+++ src/ components/Ingredients/index.tsx
@@ -41,23 +41,25 @@ const Ingredients = () => {
             {isLoading && <p>Loading ...</p>}
             <div className="flex">
                 <div className="flex-auto p-6">
-                    <div className="shadow-xl rounded-r-xl p-6">
+                    <div className="rounded-r-xl p-6 shadow-xl">
                         <div className="flex flex-wrap">
-                            <h1 className="flex-auto text-xl font-semibold my-2 mx-4">
+                            <h1 className="flex-auto mx-4 my-2 text-xl font-semibold">
                                 Ingredients
                             </h1>
                             {/* {data &&
                                 data.products?.map((product: any) => ( */}
-                                    <div className="w-full my-2">
-                                        <h2 className="flex-auto text-xl font-semibold inline-block mx-4">
-                                            3
-                                        </h2>
-                                        <p className="text-sm inline mx-4">cups of water</p>
-                                        <p className="text-sm inline mx-4">price</p>
-                                        <button className="text-sm font-semibold bg-pink-300 text-white py-3 px-4 rounded-lg hover:bg-pink-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-600 focus-visible:ring-offset-2 focus-visible:ring-offset-pink-900 mx-4">
-                                            +
-                                        </button>
-                                    </div>
+                            <div className="w-full my-2">
+                                <h2 className="flex-auto inline-block mx-4 text-xl font-semibold">
+                                    3
+                                </h2>
+                                <p className="inline mx-4 text-sm">
+                                    cups of water
+                                </p>
+                                <p className="inline mx-4 text-sm">price</p>
+                                <button className="hover:bg-red-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-600 focus-visible:ring-offset-2 focus-visible:ring-offset-pink-900 px-4 py-3 mx-4 text-sm font-semibold text-white bg-red-400 rounded-lg">
+                                    +
+                                </button>
+                            </div>
                             {/* ))} */}
                         </div>
                     </div>
