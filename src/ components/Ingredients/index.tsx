0d8d5947495916cd7c930bc51834d035cc30832e
Elena Gimmel <elena.gimmel@proeins.de>
2021-04-22 19:13:02 +0200

    Rough structure of the list of ingredients

--- src/ components/Ingredients/index.tsx
+++ src/ components/Ingredients/index.tsx
@@ -4,19 +4,21 @@ import { useQuery } from "react-query";
 const endpoint = "http://127.0.0.1/graphql";
 
 function useIngredients() {
-    return useQuery("products", async () => {
+    return useQuery("ingredients", async () => {
         const data = await request(
             endpoint,
             gql`
                 query {
-                    products {
-                        id
-                        title
-                        price {
-                            price
-                            currency {
-                                id
-                                sign
+                    recipe (id: ) {
+                        products {
+                            id
+                            title
+                            price {
+                                price
+                                currency {
+                                    id
+                                    sign
+                                }
                             }
                         }
                     }
@@ -33,25 +35,31 @@ const Ingredients = () => {
     const handelOnClick = () => {
         //redirct andere view
     };
+
     return (
         <>
             {isLoading && <p>Loading ...</p>}
             <div className="flex">
                 <div className="flex-auto p-6">
-                    <div className="flex flex-wrap">
-                        <h1 className="flex-auto text-xl font-semibold">
-                            Ingredients
-                        </h1>
-                    
-                        {data &&
-                            data.products?.map((product: any) => (
-                                <div>
-                                    <h2 className="flex-auto text-xl font-semibold">
-                                        3
-                                    </h2>
-                                    <p className="text-sm text-gray-500">cups of water</p>
-                                </div>
-                        ))}
+                    <div className="shadow-xl rounded-r-xl p-6">
+                        <div className="flex flex-wrap">
+                            <h1 className="flex-auto text-xl font-semibold my-2 mx-4">
+                                Ingredients
+                            </h1>
+                            {/* {data &&
+                                data.products?.map((product: any) => ( */}
+                                    <div className="w-full my-2">
+                                        <h2 className="flex-auto text-xl font-semibold inline-block mx-4">
+                                            3
+                                        </h2>
+                                        <p className="text-sm inline mx-4">cups of water</p>
+                                        <p className="text-sm inline mx-4">price</p>
+                                        <button className="text-sm font-semibold bg-pink-300 text-white py-3 px-4 rounded-lg hover:bg-pink-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-600 focus-visible:ring-offset-2 focus-visible:ring-offset-pink-900 mx-4">
+                                            +
+                                        </button>
+                                    </div>
+                            {/* ))} */}
+                        </div>
                     </div>
                 </div>
             </div>
