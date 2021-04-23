b56631adfc70a61191a4861da48a9ae9ac450da9
Elena Gimmel <elena.gimmel@proeins.de>
2021-04-23 10:18:36 +0200

    add navigation

--- src/ components/Header/index.tsx
+++ src/ components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Navigation from "../Navigation";
+
 type Props = {
     title: string;
     subtitle: string|React.Component;
@@ -6,8 +8,9 @@ type Props = {
 const Header: React.FC<Props> = ({ title, subtitle }) => {
     return (
         <>
-            <div  className="relative bg-fixed bg-center bg-cover bg-no-repeat h-56 " style={{backgroundImage: `url("https://cdn.bbqpit.de/wp-content/uploads/2018/12/19001134/Spaghetti-Carbonara.jpg")`}}>
-                <div className="bg-gradient-to-t from-white py-12 px-6 h-full">
+            <div  className="relative bg-fixed bg-center bg-cover bg-no-repeat h-72 " style={{backgroundImage: `url("https://cdn.bbqpit.de/wp-content/uploads/2018/12/19001134/Spaghetti-Carbonara.jpg")`}}>
+                <div className="bg-gradient-to-t from-white py-8 px-6 h-full">
+                    <Navigation />
                     <h1 className="flex-auto my-2 text-3xl font-semibold capitalize text-white mb-8">
                         {title}
                     </h1>
---
+++ src/ components/Navigation/index.tsx
@@ -0,0 +1,46 @@
+import { useHistory } from "react-router-dom";
+type Props = {
+    
+};
+const Navigation: React.FC<Props> = ({ }) => {
+    let history = useHistory();
+    return (
+        <>
+            <div className="w-full flex justify-between mb-14">
+                <button onClick={() => history.goBack()} className="hover:bg-white hover:text-red-500 focus:outline-none px-4 py-3 mx-4 text-sm font-semibold text-white bg-transparent rounded-lg">
+                    <svg 
+                        viewBox="0 0 32 32" 
+                        className="w-7 h-7" 
+                        aria-hidden="true"
+                        fill="currentColor"
+                    >
+                        <path 
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M14.19 16.005l7.869 7.868-2.129 2.129-9.996-9.997L19.937 6.002l2.127 2.129z"
+                        />
+                    </svg>
+                </button>
+                <button className="hover:bg-white hover:text-red-500 focus:outline-none px-4 py-3 mx-4 text-sm font-semibold text-white bg-transparent rounded-lg">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default Navigation;
